67992837a9d1b27067684cbe930446c9bc82ff6b
gbenamy <Giladba19@gmail.com>
2020-06-08 10:27:10 +0000

    arraysExercise

---
+++ arraysExercies.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+//leetcode
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    //bruteforce solution.
+    
+    for(int i = 0; i<numsSize-1 ; i++ ){
+        for (int j = i+1; j < numsSize; j++){
+            if (nums[i]+nums[j]==target){
+                returnSize =(int*)malloc(sizeof(int)*2);
+                returnSize[0]=i;
+                returnSize[1]=j;
+                return returnSize;
+            }
+        }
+        
+    }
+    
+    return NULL;
+
+
+}
+
+int maxSubArray(int* nums, int numsSize){
+    int max = nums[0], current = 0, val = 0, positiveFlag=0,maxNeg=nums[0];
+    for(int i = 0; i < numsSize ; i++){
+        val = nums[i];
+        if(val>0){
+            positiveFlag=1;
+        }
+        else{
+            if (maxNeg<val){
+                maxNeg=val;
+            }
+        }
+        if(val + current < 0){
+            if (current > max){
+                max=current;
+            }
+            
+            current = 0;
+        }
+        
+        else{
+            current = current + val;
+            if(max<current){
+                max = current;
+            
+            }
+        }
+        
+    }
+    if(positiveFlag){
+        return max;
+    }
+    else{
+        return maxNeg;
+    }
+
+}
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define STRSIZE 256
+//void reverseAString(char str[]);
 
 
 void arrPrint(int arr[], int len){
@@ -17,6 +18,7 @@ int main() {
 
   int arr1[] = {0,3,4,34};
   int arr2[] = {3,4,6,30};
+  int * twoSums = NULL;
   
   char str[STRSIZE] = "abc";
 
@@ -33,6 +35,10 @@ int main() {
   arrPrint(arr1,4);
   arrPrint(arr2,4);
   arrPrint(mergeSortedArrays(arr1,4,arr2,4),8);
+  
+  twoSums = twoSum(arr1, 4, 4, NULL);
+  printf("the coordinate are:[%d][%d]\n",twoSums[0],twoSums[1]);
 
+  printf("done\n");
   return 0;
 }
