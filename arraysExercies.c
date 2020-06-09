47ea66d9956f365544ddcabff4600a40d90a5a53
gbenamy <Giladba19@gmail.com>
2020-06-09 07:15:56 +0000

    added moveZeroes

--- arraysExercies.c
+++ arraysExercies.c
@@ -58,3 +58,26 @@ int maxSubArray(int* nums, int numsSize){
     }
 
 }
+
+void moveZeroes(int* nums, int numsSize){
+    
+    for(int i = 0; i < numsSize-1; i++)
+        
+        if(nums[i]==0){
+            for(int j = i+1; j < numsSize; j++){
+                if(nums[j]){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    break;
+                }
+                if(j == numsSize){
+                    return;     
+                }
+                
+            
+            }
+            
+        }
+
+}
